4a67745a6e3592c4bfabe431028d34132b250480
Khushi Khetrapal <200431715@student.georgianc.on.ca>
2020-01-23 19:49:45 -0800

    completed lab2

---
+++ LAB2/Controllers/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab2.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+
+            string[] product = { "laptop", "tablet", "Desktop" };
+            
+            ViewData["product"] = product;
+            return View();
+        }
+        public string Details(string detail)
+        {
+            string message = "You have selected" + @detail;
+            return message;
+        }
+    }
+}
+
